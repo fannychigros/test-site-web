595153d4cdad752137a994230ceedc7ca3ad03b0
fannychigros <122257806+fannychigros@users.noreply.github.com>
2025-04-11 15:57:14 +0200

    Update menu.js

--- templates/menu/menu.js
+++ templates/menu/menu.js
@@ -1,4 +1,4 @@
-fetch('menu.html')
+fetch('https://fannychigros.github.io/test-site-web/templates/menu/menu.html')
     .then(response => response.text())
     .then(html => {
       document.getElementById('menu-container').innerHTML = html;
