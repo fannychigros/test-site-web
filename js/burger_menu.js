deb3ef6a5bfbad87cbe7946ef7800b1188f8630e
fannychigros <122257806+fannychigros@users.noreply.github.com>
2025-04-29 09:53:59 +0200

    modif lien vers image baniere et suppression de fichier inutile

--- js/burger_menu.js
+++
@@ -1,9 +0,0 @@
-/*document.addEventListener("DOMContentLoaded", function () {
-  const burger = document.getElementById("burger");
-  const menu = document.getElementById("menu");
-
-  burger.addEventListener("click", function () {
-    menu.classList.toggle("open");
-  });
-});
-*/
